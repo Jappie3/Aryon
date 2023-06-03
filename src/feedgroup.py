1c6c677d0183419d2967d8fedcee619cbbea90ee
Jasper <jasper22034@gmail.com>
2023-06-03 17:16:20 +0200

    remove unused files

--- src/feedgroup.py
+++
@@ -1,8 +0,0 @@
-class FeedGroup:
-    def __init__(self, title, feeds):
-        self.title = title
-        self.feeds = feeds
-
-    def update(self):
-        for feed in self.feeds:
-            feed.update()

ccd8b5b1a2caab1216964e5179e6f3ca85ca46de
Jasper <jasper22034@gmail.com>
2023-06-04 11:13:09 +0200

    check if $2 is app.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 # if running as root & command specified in CMD is python
-if [ "$(id -u)" = "0" ] && [ "$1" = "python" ]; then
+if [ "$(id -u)" = "0" ] && [ "$1" = "python" ] && [ "$2" = "app.py" ]; then
     # make sure permissions are set correctly
     chown -R aryon:aryon /app
     cd /app
-    # run command specified in CMD (verified to be python) as aryon user
+    # run command (verified to be `python app.py`) as aryon user
     exec su-exec aryon "$@"
 fi
 
